ee01ae496f8bed06ceb846ccde88e49761d1f730
Alvaro <alvieboy@alvie.com>
2017-10-25 14:26:29 +0100

    Initial import

---
+++ Makefile
@@ -0,0 +1,118 @@
+TARGET=abobora
+
+
+all: $(TARGET).bin
+
+CROSS=arm-none-eabi-
+CC=$(CROSS)gcc
+AS=$(CROSS)gcc
+OBJCOPY=$(CROSS)objcopy
+
+CUBE=../..//STM32Cube_FW_F1_V1.4.0/
+
+HAL=$(CUBE)/Drivers/STM32F1xx_HAL_Driver/
+
+HALSRCPATH=$(HAL)/Src
+HALINCPATH=$(HAL)/Inc
+
+USBDEVICE=$(CUBE)/Middlewares/ST/STM32_USB_Device_Library/
+USBCOREINC=$(USBDEVICE)/Core/Inc
+USBCORESRC=$(USBDEVICE)/Core/Src
+
+USB_SOURCES=\
+	usbd_core.c  \
+	usbd_ctlreq.c  \
+	usbd_ioreq.c
+
+USBCDCINC=$(USBDEVICE)/Class/CDC/Inc
+USBCDCSRC=$(USBDEVICE)/Class/CDC/Src
+
+USBCDC_SOURCES=usbd_cdc.c
+
+HAL_SOURCES= \
+stm32f1xx_hal_adc.c                 \
+stm32f1xx_hal_adc_ex.c              \
+stm32f1xx_hal.c                     \
+stm32f1xx_hal_can.c                 \
+stm32f1xx_hal_cec.c                 \
+stm32f1xx_hal_cortex.c              \
+stm32f1xx_hal_crc.c                 \
+stm32f1xx_hal_dac.c                 \
+stm32f1xx_hal_dac_ex.c  	    \
+stm32f1xx_hal_dma.c                 \
+stm32f1xx_hal_eth.c                 \
+stm32f1xx_hal_flash.c               \
+stm32f1xx_hal_flash_ex.c            \
+stm32f1xx_hal_gpio.c                \
+stm32f1xx_hal_gpio_ex.c             \
+stm32f1xx_hal_hcd.c                 \
+stm32f1xx_hal_i2c.c                 \
+stm32f1xx_hal_i2s.c                 \
+stm32f1xx_hal_irda.c                \
+stm32f1xx_hal_iwdg.c                \
+stm32f1xx_hal_nand.c                \
+stm32f1xx_hal_nor.c                 \
+stm32f1xx_hal_pccard.c              \
+stm32f1xx_hal_pcd.c                 \
+stm32f1xx_hal_pcd_ex.c              \
+stm32f1xx_hal_pwr.c                 \
+stm32f1xx_hal_rcc.c                 \
+stm32f1xx_hal_rcc_ex.c              \
+stm32f1xx_hal_rtc.c                 \
+stm32f1xx_hal_rtc_ex.c              \
+stm32f1xx_hal_sd.c                  \
+stm32f1xx_hal_smartcard.c           \
+stm32f1xx_hal_spi.c                 \
+stm32f1xx_hal_spi_ex.c              \
+stm32f1xx_hal_sram.c                \
+stm32f1xx_hal_tim.c                 \
+stm32f1xx_hal_tim_ex.c              \
+stm32f1xx_hal_uart.c                \
+stm32f1xx_hal_usart.c               \
+stm32f1xx_hal_wwdg.c                \
+stm32f1xx_ll_usb.c              
+
+_HOBJ=$(HAL_SOURCES:.c=.o)
+_USBOBJ=$(USB_SOURCES:.c=.o)
+_USBCDCOBJ=$(USBCDC_SOURCES:.c=.o)
+
+HOBJ=$(patsubst %, $(HALSRCPATH)/%, $(_HOBJ))
+USBOBJ=$(patsubst %, $(USBCORESRC)/%, $(_USBOBJ))
+USBCDCOBJ=$(patsubst %, $(USBCDCSRC)/%, $(_USBCDCOBJ))
+
+CFLAGS=-O2 -Wall -mcpu=cortex-m3 -mthumb -mthumb-interwork -I$(HALINCPATH) -I. \
+-I$(CUBE)/Drivers/CMSIS/Device/ST/STM32F1xx/Include/ \
+-I$(CUBE)/Drivers/CMSIS/Include \
+-I$(USBCOREINC) \
+-I$(USBCDCINC) \
+-DSTM32F103x6 \
+-fdata-sections \
+-ffunction-sections
+
+ASFLAGS=-O2 -mcpu=cortex-m3 -mthumb -x assembler-with-cpp
+
+SRC=main.c \
+    system_stm32f1xx.c	\
+    stm32f1xx_it.c 	
+
+ 
+ASRC=startup_stm32f103x6.s
+
+AOBJ=$(ASRC:.s=.o)
+OBJS=$(SRC:.c=.o) $(AOBJ)
+
+$(TARGET).bin: $(TARGET).elf
+	$(OBJCOPY) -O binary $< $@
+
+$(TARGET).elf: $(OBJS) $(HOBJ)
+	$(CC) -o $@ $+ -Tstm32f103.ld -nostartfiles -Wl,--gc-sections -static -flto
+
+$(AOBJ) : %.o : %.s
+	$(CC) -c $(ASFLAGS) $< -o $@
+
+clean:
+	rm -f $(OBJS) $(HOBJ) $(TARGET).elf
+
+flash: $(TARGET).bin
+	sudo dfu-util -d 1eaf:0003 -c 1 -i 0 -a 2 -D $(TARGET).bin
+
---
+++ main.c
@@ -0,0 +1,162 @@
+#include <stm32f1xx_hal_gpio.h>
+#include <stm32f1xx_hal.h>
+#include <stm32f1xx_hal_dma.h>
+#include <stm32f1xx_hal_tim.h>
+#include <stm32f1xx_hal_rcc.h>
+#include <stm32f1xx_hal_spi.h>
+#include <string.h>
+
+/*
+ PA5 SPI_SCK
+ PA6 SPI_MISO
+ PA7 SPI_MOSI
+*/
+
+void Error_Handler();
+
+static void init_gpio()
+{
+    GPIO_InitTypeDef init;
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    init.Pin = GPIO_PIN_13;
+    init.Mode = GPIO_MODE_OUTPUT_PP;
+    init.Pull = GPIO_NOPULL;
+    init.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_Init( GPIOC, &init );
+
+    init.Speed = GPIO_SPEED_FREQ_HIGH;
+    init.Pin = GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
+    init.Mode = GPIO_MODE_AF_PP;
+
+    HAL_GPIO_Init( GPIOA, &init );
+}
+
+static void setupCLK(void)
+{
+    unsigned int StartUpCounter=0;
+
+    // enable HSE
+    RCC->CR |= 0x00010001;
+
+    // and wait for it to come on
+    while ((RCC->CR & 0x00020000) == 0);
+
+    // enable flash prefetch buffer
+    FLASH->ACR = 0x00000012;
+	
+    // Configure PLL
+#ifdef XTAL12M
+    RCC->CFGR |= 0x00110400; /* pll=72Mhz(x6),APB1=36Mhz,AHB=72Mhz */
+#else
+    RCC->CFGR |= 0x001D0400; /* pll=72Mhz(x9),APB1=36Mhz,AHB=72Mhz */
+#endif
+
+    // enable the pll
+    RCC->CR |= 0x01000000;
+
+#ifndef HSE_STARTUP_TIMEOUT
+  #define HSE_STARTUP_TIMEOUT    ((unsigned int)0x0500) /*!< Time out for HSE start up */
+#endif /* HSE_STARTUP_TIMEOUT */   
+
+    StartUpCounter = HSE_STARTUP_TIMEOUT;
+    while (((RCC->CR & 0x03000000) == 0) && --StartUpCounter);
+	
+	if (!StartUpCounter) {
+		// HSE has not started. Try restarting the processor
+		Error_Handler();
+	}
+	
+    // Set SYSCLK as PLL
+    RCC->CFGR |= 0x00000002;
+    // and wait for it to come on
+    while ((RCC->CFGR & 0x00000008) == 0); 
+
+    RCC->AHBENR = 0x0;
+    RCC->APB2ENR = 0x1; // AFIO only
+}
+
+
+void Error_Handler()
+{
+    int c=0;
+    while (1) {
+        c++;
+        HAL_GPIO_WritePin( GPIOC, GPIO_PIN_13, c&1);
+    }
+}
+
+static SPI_HandleTypeDef spih;
+
+static void spi_init()
+{
+    spih.Instance = SPI1;
+
+    spih.Init.Mode = SPI_MODE_MASTER;
+    spih.Init.Direction = SPI_DIRECTION_2LINES;
+    spih.Init.DataSize = SPI_DATASIZE_16BIT;
+    spih.Init.CLKPolarity = SPI_POLARITY_LOW;
+    spih.Init.CLKPhase = SPI_PHASE_1EDGE;
+    spih.Init.NSS = SPI_NSS_SOFT;
+    spih.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+    spih.Init.FirstBit = SPI_FIRSTBIT_MSB;
+    spih.Init.TIMode = SPI_TIMODE_DISABLE;
+    spih.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    spih.Init.CRCPolynomial = 0;
+
+    __HAL_RCC_SPI1_CLK_ENABLE();
+
+    HAL_SPI_Init(&spih);
+}
+
+static void spi_tx32(unsigned val)
+{
+    unsigned char temp[4];
+    unsigned char temptx[4];
+    uint16_t high = val>>16;
+    uint16_t low = val;
+
+    temptx[0] = 0;
+    temptx[1] = 0;
+    temptx[2] = 0;
+    temptx[3] = 0;
+
+    HAL_SPI_TransmitReceive(&spih, &high, temp, 1, 1000);
+    HAL_SPI_TransmitReceive(&spih, &low, temp,  1, 1000);
+}
+
+int main()
+{
+    setupCLK();
+    SystemCoreClockUpdate();
+
+    HAL_Init();
+
+    init_gpio();
+    spi_init();
+    int i;
+
+    for (i=0;i<1024/64;i++) {
+        spi_tx32(0x00000000);
+    }
+    // Clear
+    for (i=0;i<1024;i++) {
+        spi_tx32(0xFF00000);
+    }
+    for (i=0;i<1024/64;i++) {
+        spi_tx32(0x00000000);
+    }
+
+    while (1) {
+        spi_tx32(0xFF0101FE);
+        spi_tx32(0xFF01FE01);
+        spi_tx32(0xFFFE0101);
+        spi_tx32(0xFF808080);
+        spi_tx32(0x00000000);
+        HAL_Delay(2);
+    }
+}
